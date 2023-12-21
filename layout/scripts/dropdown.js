51376da4f4f595e536e906d27c0c14ab8f84568a
Aoichan26 <153802543+Aoichan26@users.noreply.github.com>
2023-12-21 16:42:47 +0800

    Update dropdown.js
    试图修改option1无法展开的bug

--- layout/scripts/dropdown.js
+++ layout/scripts/dropdown.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.menu-title').forEach(function(title) {
+  document.querySelectorAll('.menu .menu-title').forEach(function(title) {
     title.addEventListener('click', function() {
       var parentLi = this.parentNode;
-      if (parentLi.classList.contains('menu-item')) {
-        // 切换当前菜单项的展开/收起状态
-        parentLi.classList.toggle('open');
-      }
+      parentLi.classList.toggle('open');
     });
   });
 });
+
 
