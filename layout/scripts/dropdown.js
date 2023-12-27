c26dc5263ac4596e0c2bf2a0472acf28a2b7d630
Aoichan26 <153802543+Aoichan26@users.noreply.github.com>
2023-12-27 23:28:37 +0800

    Update dropdown.js
    新的菜单js

--- layout/scripts/dropdown.js
+++ layout/scripts/dropdown.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.menu .menu-title').forEach(function(title) {
-    title.addEventListener('click', function() {
-      var parentLi = this.parentNode;
-      parentLi.classList.toggle('open');
+document.querySelectorAll('.menu-title').forEach(item => {
+    item.addEventListener('click', function() {
+        const target = document.querySelector(this.getAttribute('data-target'));
+        if (target) {
+            target.style.display = target.style.display === 'none' ? 'block' : 'none';
+            this.querySelector('.menu-icon').style.transform = target.style.display === 'none' ? 'rotate(0deg)' : 'rotate(90deg)';
+        }
     });
-  });
 });
 
 
