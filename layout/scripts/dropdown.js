d1c8523cebea7b1dba62756c5a26ab8f1aff8a12
Aoichan26 <153802543+Aoichan26@users.noreply.github.com>
2023-12-28 00:42:00 +0800

    Update dropdown.js
    把>>改成三角形

--- layout/scripts/dropdown.js
+++ layout/scripts/dropdown.js
@@ -3,9 +3,9 @@ document.querySelectorAll('.menu-title').forEach(item => {
         const target = document.querySelector(this.getAttribute('data-target'));
         if (target) {
             target.style.display = target.style.display === 'none' ? 'block' : 'none';
+            // 根据菜单的展开状态旋转箭头
             this.querySelector('.menu-icon').style.transform = target.style.display === 'none' ? 'rotate(0deg)' : 'rotate(90deg)';
         }
     });
 });
-
 
