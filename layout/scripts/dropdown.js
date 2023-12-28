704b7fa8161b71fa0fcc3d416e13beedd7590420
Aoichan26 <153802543+Aoichan26@users.noreply.github.com>
2023-12-28 16:29:29 +0800

    Update dropdown.js
    修改了Js以解决第一次单击无法展开菜单的问题

--- layout/scripts/dropdown.js
+++ layout/scripts/dropdown.js
@@ -1,11 +1,19 @@
-document.querySelectorAll('.menu-title').forEach(item => {
-    item.addEventListener('click', function() {
-        const target = document.querySelector(this.getAttribute('data-target'));
-        if (target) {
-            target.style.display = target.style.display === 'none' ? 'block' : 'none';
-            // 根据菜单的展开状态旋转箭头
-            this.querySelector('.menu-icon').style.transform = target.style.display === 'none' ? 'rotate(0deg)' : 'rotate(90deg)';
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    // 设置每个子菜单的初始隐藏状态
+    document.querySelectorAll('.sub-menu').forEach(subMenu => {
+        subMenu.style.display = 'none';
+    });
+
+    // 绑定点击事件
+    document.querySelectorAll('.menu-title').forEach(item => {
+        item.addEventListener('click', function() {
+            const target = document.querySelector(this.getAttribute('data-target'));
+            if (target) {
+                target.style.display = target.style.display === 'none' ? 'block' : 'none';
+                this.querySelector('.menu-icon').style.transform = target.style.display === 'none' ? 'rotate(0deg)' : 'rotate(90deg)';
+            }
+        });
     });
 });
+
 
